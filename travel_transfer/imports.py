11b01e307a26f50c87fcb2768313cfc93580fcd3
Yuri Quintana <yuriq@uh.cu>
2015-05-08 12:00:24 -0400

    Fixing a problem when merged

--- travel_transfer/imports.py
+++ travel_transfer/imports.py
@@ -72,7 +72,7 @@ class import_transfer(TransientModel):
 
     def read_from_calc(self, data, sheet, context=None):
         document = xlrd.open_workbook(file_contents=data)
-        data = document.sheet_by_index(sheet)
+        data = document.sheets()
         return data
 
     def get_id_by_name(self, cr, uid, model, name, context=None):
@@ -105,49 +105,147 @@ class import_transfer(TransientModel):
     def find_by_code(self, cr, uid, code, model, context=None):
         obj = self.pool.get(model)
         val = obj.search(cr, uid, [('code', '=', code)], context=context)[0]
-        return val
+        return val  
+       
+    def find_by_name(self, cr, uid, name, model, context=None):
+        obj = self.pool.get(model)
+        val = obj.search(cr, uid, [('name', '=', code)], context=context)[0]
+        return val 
+    
+    def get_option_value(self, cr, uid, name, code, context=None):
+        ot = self.pool.get('option.type')
+        ov = self.pool.get('option.value')
 
+        ot_id = ot.search(cr, uid, [('code', '=', code)], context=context)[0]
+        to_search = [('name', '=', name), ('option_type_id', '=', ot_id)]
+        ov_ids = ov.search(cr, uid, to_search, context=context)
+        if ov_ids:
+            return ov_ids[0]
+        else:
+            raise Exception()
+    
     ''' Transfers '''
 
     def load_transfer(self, cr, uid, obj, data, context=None):
         product_transfer = self.pool.get('product.transfer')
         product_supplierinfo = self.pool.get('product.supplierinfo')
         pricelist_partnerinfo = self.pool.get('pricelist.partnerinfo')
+        partner = self.pool.get('res.partner')
+        
+        msg = ''
+        sheet = data[-1]
+        
+        head = {sheet.cell_value(1, x): x for x in range(sheet.ncols)} 
+        
+        date_from = sheet.cell_value(0, 0)
+        date_to   = sheet.cell_value(0, 1)
+    
+        for r in range(2, sheet.nrows):
+            
+            def cell(attr):
+                if sheet.cell(r, head[attr]).ctype == xlrd.XL_CELL_ERROR:
+                    return None
+                return sheet.cell_value(r, head[attr])
+            
+            suppinfo_id = False
+            transfer_obj = False
+            min_paxs = False
+            max_paxs = False
+            vehicle_type_id = False
+            guide_id = False
+            confort_id = False
+            price = False
+            
+            if cell('Name'):                                
+                transfer_name = cell('Name').strip()
+                category_id = self.get_categ_id(cr, uid, 'Transfer', context)
+                transfer_ids = product_transfer.search(cr, uid, [('name', '=', transfer_name)])
+                if len(transfer_ids) == 0:
+                    transfer_id = product_transfer.create(cr, uid, {'name': transfer_name}, context)
+                elif len(transfer_ids) > 1: 
+                    msg += 'Ambiguous name for transfer: '+ transfer_name + '\n'
+                    continue
+                else:
+                    transfer_obj = product_transfer.browse(cr, uid, transfer_ids[0], context)
+                
+            if cell('Supplier'):
+                if transfer_obj:
+                   suppinfo_id = None
+                   supplier_name = str(cell('Supplier')).strip()
+                   if supplier_name != '':
+                       partner_ids = partner.search(cr, uid, [('name', '=', supplier_name)])
+                       if len(partner_ids) == 0:
+                           msg += 'Supplier name not found: '+ supplier_name + '\n'
+                           continue
+                       elif len(partner_ids) > 1: 
+                           msg += 'Ambiguous name for supplier: '+ supplier_name + '\n'
+                           continue
+                       else:
+                           partner_id = partner_ids[0]
+                           suppinfo_ids = product_supplierinfo.search(cr, uid, ['&', 
+                                                                                ('name', '=', partner_id), 
+                                                                                ('product_tmpl_id', '=', transfer_obj.product_tmpl_id.id)], 
+                                                                      context=context)
+                           if len(suppinfo_ids) == 0:        
+                               svals = {
+                                   'name': partner_id,
+                                   'product_tmpl_id': transfer_obj.product_tmpl_id.id,
+                                   'min_qty': 0
+                               }
+                               suppinfo_id = product_supplierinfo.create(cr, uid, svals, context)
+                           else:
+                               suppinfo_id = suppinfo_ids[0] 
+                               
+            if cell('Min paxs'):
+                try: 
+                    min_paxs = int(cell('Min paxs'))
+                except:
+                    msg += 'Wrong min paxs for: '+ transfer_name + '\n'
+                    continue
+            if cell('Max paxs'):
+                try:
+                    max_paxs = int(cell('Max paxs'))
+                except:
+                    msg += 'Wrong max paxs for: '+ transfer_name + '\n'
+                    continue
+            if cell('Vehicle type'):
+                try:
+                    vehicle_type_id = self.get_option_value(cr, uid, cell('Vehicle type'), 'vt', context).upper()
+                except:
+                    msg += 'Wrong vehicle type option in '+ transfer_name + '\n'
+                    continue
+            if cell('Guide'):
+                try:
+                    guide_id = self.get_option_value(cr, uid, cell('Guide'), 'guide', context).upper()
+                except:
+                    msg += 'Wrong guide option in '+ transfer_name + '\n'
+                    continue
+            if cell('Confort'):
+                try:
+                    confort = self.get_option_value(cr, uid, cell('Confort'), 'vc', context).upper()
+                except:
+                    msg += 'Wrong confort option in '+ transfer_name + '\n'
+                    continue
+            if cell('Price'):
+                try:
+                    price = float(cell('Price'))
+                except:
+                    msg += 'Wrong price for: '+ transfer_name + '\n'
+                    continue
 
-        dict_options = self.prepare_load(cr, uid, context)
-        for d in range(6, data.nrows):
-            name = (data.cell_value(d, 0) + ' - ' + data.cell_value(d, 1)).encode('UTF-8')
-            transfer_ids = product_transfer.search(cr, uid, [('name', '=', name)], context=context)
-            if transfer_ids:
-                transfer = product_transfer.browse(cr, uid, transfer_ids[0], context)
-            else:
-                context.update({'category': 'transfer'})
-                transfer_id = product_transfer.create(cr, uid, {'name': name}, context)
-                transfer = product_transfer.browse(cr, uid, transfer_id)
-            seller_ids = [s.id for s in transfer.seller_ids]
-            if seller_ids:
-                product_supplierinfo.unlink(cr, uid, seller_ids, context)
-            svals = {
-                'name': obj.supplier_id.id,
-                'product_tmpl_id': transfer.product_id.product_tmpl_id.id,
-                'min_qty': 0
-            }
-            old_suppinfo_id = product_supplierinfo.search(cr, uid, 
-                                                          [('name', '=', obj.supplier_id.id),
-                                                           ('product_tmpl_id', '=', transfer.product_id.product_tmpl_id.id)])
-            product_supplierinfo.unlink(cr, uid, old_suppinfo_id)
-            suppinfo_id = product_supplierinfo.create(cr, uid, svals, context)
-            for k, v in dict_options.iteritems():
-                price = data.cell_value(d, k)
-                pvals = {
-                    'start_date': obj.start_date,
-                    'end_date': obj.end_date,
-                    'price': price,
-                    'min_quantity': 0,
-                    'suppinfo_id': suppinfo_id
-                }
-                pvals.update(v)
-                pricelist_partnerinfo.create(cr, uid, pvals, context)
+            if suppinfo_id and transfer_obj and min_paxs and max_paxs and vehicle_type and guide and confort and price:
+                pvals = {'start_date': date_from,
+                         'end_date': date_to,
+                         'price': price,
+                         'min_quantity': 0,
+                         'suppinfo_id': suppinfo_id,
+                         'min_paxs': min_paxs,
+                         'max_paxs': max_paxs,
+                         'vehicle_type_id': vehicle_type,
+                         'guide_id': guide,
+                         'confort_id': confort_id
+                         }
+            pricelist_partnerinfo.create(cr, uid, pvals, context)
         return True
 
     def prepare_load(self, cr, uid, context):
